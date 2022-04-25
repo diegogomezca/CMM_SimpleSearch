a4c272f60af0e259ab0dc9146545bb1c3f1ce06a
diegogomezca <51853402+diegogomezca@users.noreply.github.com>
2022-04-25 18:11:39 +0200

    Add files via upload

---
+++ simpleSearch.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+def simple_search(metabolites_type, databases, masses_mode, ion_mode,adducts,tolerance,tolerance_mode,masses):
+
+	url = "http://ceumass.eps.uspceu.es/mediator/api/v3/batch"
+
+	x = {
+	"metabolites_type": "all-except-peptides",
+	"databases": ["hmdb"],
+	"masses_mode": "mz",
+	"ion_mode": "positive",
+	"adducts": ["all"],
+	"tolerance": 10.0,
+	"tolerance_mode": "ppm",
+	"masses": [400.3432]
+	}
+
+	out = requests.post(url,json = x)
+	json = out.json()
+
+	return json
