91f1e38c9c81bfff8177378dc0f7a1c39bfeee6e
Diego Gómez Calles <diegogomezca00@gmail.com>
2022-05-16 19:23:56 +0200

    160522

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
 
-setuptools.setup(name='cmmsimplesearch',
+setuptools.setup(
+      name='cmmsimplesearch',
       version='1.0.0',
       description='CEU Mass Mediator Tool',
       url='https://github.com/diegogomezca/CMM_SimpleSearch',
       author='Diego Gomez',
-      packages=setuptools.find_packages(where="src")
+      packages=['src'],
 )
