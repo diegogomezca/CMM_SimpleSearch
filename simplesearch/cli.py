a0fc34c764078401862e91c013290f85f4696ad5
Diego Gómez Calles <diegogomezca00@gmail.com>
2022-05-25 17:45:52 +0200

    250522

--- simplesearch/cli.py
+++ simplesearch/cli.py
@@ -5,7 +5,7 @@ def main():
 	parser = argparse.ArgumentParser(description='CEU Mass Mediator is a tool for searching metabolites in different databases')
 	parser.add_argument('--version', action='version', version='1.0')
 	args = parser.parse_args()
-	print(args)
+	return args
 
 
 if __name__ == '__main__':
