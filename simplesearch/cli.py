806abdd10016835b6b2d7d6f87affb5b411298e0
Diego Gómez Calles <diegogomezca00@gmail.com>
2022-06-13 08:59:49 +0200

    130622

--- simplesearch/cli.py
+++ simplesearch/cli.py
@@ -2,10 +2,17 @@ import argparse
 import requests
 import json
 
+def myEntryPoint(query_type, atabases, masses_mode, ion_mode, adducts, tolerance, tolerance_mode, masses)):
+	if(query_type == "SIMPLE_SEARCH"):
+		return simple_search(json_data)
+	else if(query_type == "BATCH:_SEARCH"):
+		# CAMBIAR A BATCH SEARCH return simple_search(json_data)
+
 def simple_search(metabolites_type, databases, masses_mode, ion_mode,adducts,tolerance,tolerance_mode,masses):
 
-	url = "http://ceumass.eps.uspceu.es/mediator/api/v3/batch"
+	print(metabolites_type, databases, masses_mode, ion_mode,adducts,tolerance,tolerance_mode,masses)
 
+	url = "http://ceumass.eps.uspceu.es/mediator/api/v3/batch"
 	x = {
 	"metabolites_type": "all-except-peptides",
 	"databases": ["hmdb"],
@@ -18,15 +25,18 @@ def simple_search(metabolites_type, databases, masses_mode, ion_mode,adducts,tol
 	}
 
 	out = requests.post(url,json = x)
+	# No se puede asumir que es correcto
+	print(out.status_code)
+
 	json = out.json()
 
 	return json
 
 
 def main():
-	print("TESTING MY MAIN FUNCTION")
+	
 
-		
+
 	parser = argparse.ArgumentParser(description='CEU Mass Mediator is a tool for searching metabolites in different databases')
 
 	parser.add_argument('--version', action='version', version='1.0')
@@ -40,10 +50,10 @@ def main():
 	parser.add_argument("masses", type=float)
 
 	args = parser.parse_args()
-
+	# SWITCH CASE CON QUERY TYPE 
 	outjson = simple_search(args.metabolites_type,args.databases,args.masses_mode,args.ion_mode,args.adducts,args.tolerance,args.tolerance_mode,args.masses)
 
-	print(outjson)
+	return(outjson)
 
 if __name__ == '__main__':
     main()
