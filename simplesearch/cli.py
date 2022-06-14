e665ada2f75b44ab69f7612b7ca78d3016a5cbb9
diegogomezca <51853402+diegogomezca@users.noreply.github.com>
2022-06-14 10:29:07 +0200

    Add files via upload

--- simplesearch/cli.py
+++ simplesearch/cli.py
@@ -41,8 +41,6 @@ def main():
 	args = parser.parse_args()
 
 	out = simple_search(args.metabolites_type,args.databases,args.masses_mode,args.ion_mode,args.adducts,args.tolerance,args.tolerance_mode,args.masses)
-	print("Imprimiendo out:")
-	print(out)
 	return out
 
 if __name__ == '__main__':
